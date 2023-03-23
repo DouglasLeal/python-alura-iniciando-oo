fadba3bd54552ecac3f9b83a30a9657ddb6e0093
Douglas Leal <douglas.leal@live.com>
2023-03-23 10:17:59 -0300

    criando método transferir

--- conta.py
+++ conta.py
@@ -1,15 +1,19 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f"Saldo {self.saldo} do titular {self.titular}")
+        print(f"Saldo {self.__saldo} do titular {self.__titular}")
 
-    def deposita(self, valor):
-        self.saldo += valor
+    def depositar(self, valor):
+        self.__saldo += valor
 
     def sacar(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transferir(self, valor, destinho):
+        self.sacar(valor)
+        destinho.depositar(valor)
--- teste.py
+++ teste.py
@@ -1,5 +1,9 @@
 from conta import Conta
 
 c1 = Conta(123, "Douglas", 100, 1000)
+c2 = Conta(123, "Ana", 100, 1000)
+
+c1.transferir(50, c2)
 
 c1.extrato()
+c2.extrato()
